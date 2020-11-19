7760320e327e4255356e7f5298b56a57a04e37bf
Dominik Pilár <445537@mail.muni.cz>
2020-11-19 14:41:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,25 +5,13 @@ setup(
     name='mkdocs-swagger-pages-plugin',
     version='1.0.0',
     description='A MkDocs plugin to render yaml files as swagger UI.',
-    long_description='',
+    long_description=read('README.md'),
     keywords='mkdocs swagger yaml',
-    author='Dominik Pilar',
-    license='MIT',
+    author='KYPO Team',
     python_requires='>=2.7',
     install_requires=[
         'mkdocs>=1.0.4'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'PyYAML>=5.1'
     ],
     packages=find_packages(),
     entry_points={
